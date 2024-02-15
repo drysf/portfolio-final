62708423d59af472f90aa7d79f053a22745ded8d
drysf <147316618+drysf@users.noreply.github.com>
2024-02-15 16:35:17 +0100

    Update db.php

--- portfolio/asset/back-end/db.php
+++ portfolio/asset/back-end/db.php
@@ -1,12 +1,9 @@
 <?php
-// $servername = "db5015084016.hosting-data.io";
-// $username = "dbu509845";
-// $password = "Caramel9278&";
-// $dbname = "dbs12524685";
 
-$servername = "localhost";
-$username = "root";
-$password = "123";
+
+$servername = "";
+$username = "";
+$password = "";
 $dbname = "portfolio";
 
 try {
